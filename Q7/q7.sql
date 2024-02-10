72a413c35727ae1be1a56eeec388d68dc803257c
Dricmoy <dricbhattacharjee@gmail.com>
2024-02-10 01:32:28 -0700

    blah

--- Q7/q7.sql
+++ Q7/q7.sql
@@ -1,6 +1,17 @@
-SELECT b.book_id, b.title, b.pyear
-FROM books b
-JOIN borrowings br ON b.book_id = br.book_id
-GROUP BY b.book_id, b.title, b.pyear
-ORDER BY COUNT(br.bid) DESC, b.book_id
-LIMIT 3;
+WITH BookRank AS (
+  SELECT
+    b.book_id,
+    b.title,
+    b.pyear,
+    COUNT(br.bid) AS borrow_count,
+    DENSE_RANK() OVER (ORDER BY COUNT(br.bid) DESC) AS ranking
+  FROM
+    books b
+    JOIN borrowings br ON b.book_id = br.book_id
+  GROUP BY
+    b.book_id, b.title, b.pyear
+)
+
+SELECT book_id, title, pyear
+FROM BookRank
+WHERE ranking <= 3;
