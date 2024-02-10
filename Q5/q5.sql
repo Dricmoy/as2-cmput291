f8a0ad8e983fd1226ba84680d55819274dcd5656
Dricmoy <dricbhattacharjee@gmail.com>
2024-02-10 00:23:46 -0700

    github

--- Q5/q5.sql
+++ Q5/q5.sql
@@ -1,7 +1,7 @@
-SELECT Reviews.book_id, Books.title, AVG(Reviews.rating) AS avg_rating
-FROM Reviews
-JOIN Books ON Reviews.book_id = Books.book_id
-GROUP BY Reviews.book_id
-HAVING COUNT(Reviews.review_id) >= 2
+SELECT r.book_id, b.title, AVG(r.rating) AS avg_rating
+FROM reviews r
+JOIN books b ON r.book_id = b.book_id
+GROUP BY r.book_id
+HAVING COUNT(r.rid) >= 2
 ORDER BY avg_rating DESC
 LIMIT 3;
