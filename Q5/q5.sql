e7e0619f92be1f4f1e25b6bdc70c1023af29430b
Dricmoy <dricbhattacharjee@gmail.com>
2024-02-10 00:27:54 -0700

    Q5 trying to test

--- Q5/q5.sql
+++ Q5/q5.sql
@@ -1,7 +1,7 @@
-SELECT r.book_id, b.title, AVG(r.rating) AS avg_rating
-FROM reviews r
-JOIN books b ON r.book_id = b.book_id
-GROUP BY r.book_id
-HAVING COUNT(r.rid) >= 2
+SELECT R.book_id, B.title, AVG(R.rating) AS avg_rating
+FROM reviews R
+JOIN books B ON R.book_id = B.book_id
+GROUP BY R.book_id, B.title
+HAVING COUNT(R.rid) >= 2
 ORDER BY avg_rating DESC
 LIMIT 3;
