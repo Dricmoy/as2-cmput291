6a5bdfccdd25822772191012af2498cda1e2a4db
Dricmoy <dricbhattacharjee@gmail.com>
2024-02-10 01:39:59 -0700

    Q8 test continue

--- Q8/q8.sql
+++ Q8/q8.sql
@@ -1,6 +1,6 @@
 SELECT m.email,
        COUNT(p.pid) AS total_penalties,
-       COUNT(CASE WHEN p.paid_amount >= p.amount THEN 1 END) AS paid_penalties,
+       COUNT(CASE WHEN p.paid_amount >= p.amount THEN p.pid END) AS paid_penalties,
        COALESCE(SUM(CASE WHEN p.paid_amount >= p.amount THEN p.paid_amount ELSE 0 END), 0) AS total_paid_amount
 FROM members m
 LEFT JOIN penalties p ON m.email = p.member
