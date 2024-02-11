acee4d95a3293521c5481a296342ba3ec38c54b0
Dricmoy <dricbhattacharjee@gmail.com>
2024-02-10 20:43:57 -0700

    Peter bad

--- Q9/q9.sql
+++ Q9/q9.sql
@@ -1,19 +1,18 @@
 CREATE VIEW book_info AS
-SELECT b.book_id,
-       b.title,
-       COALESCE(COUNT(DISTINCT r.rid), 0) AS revcnt,
-       COALESCE(AVG(r.rating), 0) AS rating,
-       COALESCE(AVG(CASE WHEN strftime('%Y', r.rdate) = '2023' THEN r.rating END), 0) AS rating23,
-       COALESCE(bwc.borrow_wait_count, 0) AS reqcnt
-FROM books b
-LEFT JOIN reviews r ON b.book_id = r.book_id
-LEFT JOIN (
-    SELECT book_id, COUNT(DISTINCT bid) + COUNT(DISTINCT wid) AS borrow_wait_count
-    FROM (
-        SELECT book_id, bid, NULL AS wid FROM borrowings
-        UNION ALL
-        SELECT book_id, NULL AS bid, wid FROM waitlists
-    ) AS combined
-    GROUP BY book_id
-) AS bwc ON b.book_id = bwc.book_id
-GROUP BY b.book_id, b.title;
+SELECT
+    b.book_id,
+    b.title,
+    COUNT(r.rid) AS revcnt,
+    AVG(r.rating) AS rating,
+    AVG(CASE WHEN strftime('%Y', r.rdate) = '2023' THEN r.rating ELSE NULL END) AS rating23,
+    COUNT(DISTINCT COALESCE(borrowings.bid, waitlists.wid)) AS reqcnt
+FROM
+    books b
+LEFT JOIN
+    reviews r ON b.book_id = r.book_id
+LEFT JOIN
+    borrowings ON b.book_id = borrowings.book_id
+LEFT JOIN
+    waitlists ON b.book_id = waitlists.book_id
+GROUP BY
+    b.book_id;
