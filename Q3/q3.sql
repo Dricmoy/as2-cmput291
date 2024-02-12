63fd896962a71b4e120c4f7bc9b23a45f107aa54
Dricmoy <dricbhattacharjee@gmail.com>
2024-02-12 00:37:58 -0700

    q3 without nesting

--- Q3/q3.sql
+++ Q3/q3.sql
@@ -1,9 +1,6 @@
 SELECT B.bid, M.email AS borrower_email
 FROM borrowings B
 JOIN members M ON B.member = M.email
+LEFT JOIN waitlists W ON B.book_id = W.book_id AND W.priority >= 5
 WHERE julianday(B.end_date) - julianday(B.start_date) > 14
-  AND B.book_id NOT IN (
-    SELECT W.book_id
-    FROM waitlists W
-    WHERE W.priority >= 5
-);
+  AND W.wid IS NULL;
