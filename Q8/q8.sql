492a0337e63dbd96adf610d7754732b6a6d433c4
Dricmoy <dricbhattacharjee@gmail.com>
2024-02-12 01:24:15 -0700

    q8 checked

--- Q8/q8.sql
+++ Q8/q8.sql
@@ -1,16 +1,9 @@
 SELECT
     M.email,
-    COALESCE(total_penalties, 0) AS total_penalties,
-    COALESCE(paid_penalties, 0) AS paid_penalties,
-    COALESCE(total_paid_amount, 0) AS total_paid_amount
+    COALESCE(COUNT(DISTINCT P.pid), 0) AS total_penalties,
+    COALESCE(COUNT(DISTINCT CASE WHEN P.paid_amount >= P.amount THEN P.pid END), 0) AS paid_penalties,
+    COALESCE(SUM(CASE WHEN P.paid_amount >= P.amount THEN P.amount ELSE 0 END), 0) AS total_paid_amount
 FROM members M
-LEFT JOIN (
-    SELECT
-        B.member AS email,
-        COUNT(P.pid) AS total_penalties,
-        COUNT(CASE WHEN P.paid_amount >= P.amount THEN 1 END) AS paid_penalties,
-        SUM(CASE WHEN P.paid_amount >= P.amount THEN P.amount ELSE 0 END) AS total_paid_amount
-    FROM borrowings B
-    LEFT JOIN penalties P ON B.bid = P.bid
-    GROUP BY B.member
-) PS ON M.email = PS.email;
+LEFT JOIN borrowings B ON M.email = B.member
+LEFT JOIN penalties P ON B.bid = P.bid
+GROUP BY M.email;
