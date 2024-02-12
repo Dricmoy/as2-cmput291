c5a7750e863a02751f96050a03778a71e5e4e558
Dricmoy <dricbhattacharjee@gmail.com>
2024-02-12 00:32:33 -0700

    bru

--- Q8/q8.sql
+++ Q8/q8.sql
@@ -1,7 +1,7 @@
-SELECT DISTINCT m.email,
-                COUNT(p.pid) AS total_penalties,
-                SUM(CASE WHEN p.paid_amount >= p.amount THEN 1 ELSE 0 END) AS paid_penalties,
-                SUM(CASE WHEN p.paid_amount >= p.amount THEN p.amount ELSE 0 END) AS total_paid_amount
+SELECT m.email,
+       COUNT(DISTINCT p.pid) AS total_penalties,
+       SUM(CASE WHEN p.paid_amount >= p.amount THEN 1 ELSE 0 END) AS paid_penalties,
+       SUM(CASE WHEN p.paid_amount >= p.amount THEN p.amount ELSE 0 END) AS total_paid_amount
 FROM members m
 LEFT JOIN borrowings b ON m.email = b.member
 LEFT JOIN penalties p ON b.bid = p.bid
