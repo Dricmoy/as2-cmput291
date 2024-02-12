40c648792fd22df1c5164371e714dc6d7aaab2e5
Dricmoy <dricbhattacharjee@gmail.com>
2024-02-12 04:01:12 -0700

    WORK MAN

--- Q9/q9.sql
+++ Q9/q9.sql
@@ -4,15 +4,15 @@ SELECT
     b.title,
     COALESCE(COUNT(DISTINCT r.rid), 0) AS revcnt,
     COALESCE(AVG(r.rating), 0) AS rating,
-    COALESCE(AVG(CASE WHEN strftime('%Y', r.rdate) = '2023' THEN r.rating ELSE NULL END), 0) AS rating23,
-    COALESCE(SUM(COALESCE(w.req_count, 0) + COALESCE(bo.borrow_count, 0)), 0) AS reqcnt
+    COALESCE(AVG(CASE WHEN strftime('%Y', r.rdate) = '2023' THEN r.rating END), 0) AS rating23,
+    COALESCE(SUM(COALESCE(w.waitlist_count, 0) + COALESCE(bo.borrow_count, 0)), 0) AS reqcnt
 FROM
     books b
 LEFT JOIN
     reviews r ON b.book_id = r.book_id
 LEFT JOIN
     (
-        SELECT book_id, COUNT(*) as req_count
+        SELECT book_id, COUNT(*) as waitlist_count
         FROM waitlists
         GROUP BY book_id
     ) w ON b.book_id = w.book_id
