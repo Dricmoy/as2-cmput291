14355e7954867cdc958370a2cd0bdc4126d84952
Dricmoy Bhattacharjee <70481917+Dricmoy@users.noreply.github.com>
2024-02-18 02:36:38 -0700

    Final update q1.sql

--- Q1/q1.sql
+++ Q1/q1.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT M.name, M.email
-FROM members M
-JOIN borrowings B ON M.email = B.member
-JOIN waitlists W ON M.email = W.member AND B.book_id = W.book_id;
+SELECT DISTINCT members.name, members.email
+FROM members
+JOIN borrowings ON members.email = borrowings.member
+JOIN waitlists ON members.email = waitlists.member AND borrowings.book_id = waitlists.book_id;
